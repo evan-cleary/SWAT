99d1ee764a9cdef5da89874362f08efdf5d132b8
mastershake71 <evan.cleary@hotmail.com>
2013-04-01 15:21:03 -0400

    Allow the retrieval of the current instance's battleground object.

--- src/com/division/swat/core/SWAT.java
+++ src/com/division/swat/core/SWAT.java
@@ -15,6 +15,7 @@ public class SWAT extends JavaPlugin {
     private SWConfig config;
     private RegionFile regionFile;
     public static SWAT instance;
+    private SWATBattleground swbg;
 
     @Override
     public void onEnable() {
@@ -24,7 +25,11 @@ public class SWAT extends JavaPlugin {
         this.regionFile.load();
         this.config.load();
         SWATGametype gametype = new SWATGametype(ChatColor.RED + "[" + ChatColor.DARK_AQUA + "SWAT" + ChatColor.RED + "] " + ChatColor.GREEN + "%s" + ChatColor.GRAY + " has won %s to %s", 3);
-        SWATBattleground swbg = new SWATBattleground("SWAT", gametype, regionFile.getRegion());
+        swbg = new SWATBattleground("SWAT", gametype, regionFile.getRegion());
         BattlegroundCore.getInstance().getRegistrar().registerBattleground(this, swbg);
+    }
+    
+    public SWATBattleground getBattleground(){
+        return swbg;
     }
 }
